1e7332ca8ea1986fd74af71bf74680d2a447936f
Mrufaihi <113942663+Mrufaihi@users.noreply.github.com>
2024-07-31 10:26:08 +0300

    Digital Clock (Optimized)
    fixed infinite loop and fast updates using (setInterval)

--- my-react-app/src/components/DigitalClock.jsx
+++ my-react-app/src/components/DigitalClock.jsx
@@ -2,22 +2,42 @@ import { useEffect, useState } from 'react';
 
 function DigitalClock() {
   const [time, setTime] = useState(new Date()); //intial value is current date obj
-  function handleTimeChange() {
-    setTime((t) => (t = new Date()));
-  }
+
+  // function handleTimeChange() {
+  //   setTime((t) => {
+  //     t.inter = new Date();
+  //   });
+  //   console.log('time updated');
+  // }
+
   function formatedTime() {
-    const hours = String(time.getHours()).padStart(2, '0'); //string always length 2 , if needed add '0' if empty.
+    let hours = String(time.getHours()).padStart(2, '0'); //string always length 2 , if needed add '0' if empty.
     const minutes = String(time.getMinutes()).padStart(2, '0');
     const seconds = String(time.getSeconds()).padStart(2, '0');
-    return `${hours} : ${minutes} : ${seconds}`;
+
+    const meridiem = hours >= 12 ? 'PM' : 'AM'; //Excutes before switching to 12 hours clock
+
+    hours = hours % 12 || 12; //if hours =13, (13%12 = 1 AM) | (12%12= 0 => 12AM)
+
+    return `${hours} : ${minutes} : ${seconds} ${meridiem}`;
   }
 
-  useEffect(handleTimeChange, [time]);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+      console.log('time updated');
+    }, 1000); //every 1000ms excute this code
+
+    //clean up when dismount
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return (
     <div className="clock-container">
       <div className="digital-clock">
-        <span onChange={handleTimeChange}>{formatedTime()}</span>
+        <span className="clock-background">{formatedTime()}</span>
       </div>
     </div>
   );
